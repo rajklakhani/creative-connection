c44ef52d3af61c1747229b57a363604e22beff5f
rajklakhani <lakhanirajkumar178@gmail.com>
2024-05-05 13:27:29 +0500

    this is new project

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-icon").addEventListener("click", function () {
+  let navigation = document.getElementsByClassName("navigation")[0];
+  navigation.classList.toggle("active");
+});
